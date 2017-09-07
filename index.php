3c0b9b584530011eeb110d8be0d3a2f895145a69
moradsimplon <morad.bzioui@laposte.net>
2017-09-07 14:05:08 +0200

    first commit

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
   <link rel="icon" href="favicon.png">
+  <link rel="stylesheet" href="css/bootstrap.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.3.2/css/mdb.min.css" />
   <link rel="stylesheet" href="css/normalize.css">
   <link rel="stylesheet" href="css/main.css">
@@ -25,6 +26,7 @@
   <script src="js/plugins.js"></script>
   <script src="js/jquery-3.2.1.js"></script>
   <script src="js/tether.min.js"></script>
+  <script type="text/javascript" src='js/bootstrap.js'></script>
   <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.3.2/js/mdb.min.js"></script>
   <script src="js/main.js"></script>
 <!-- ############## JS sources ############## -->
