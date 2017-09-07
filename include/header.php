f225ceed55595faef6ea96469b359b15c2a3f57a
Greg Fauconnier <g.fauconnier@hotmail.fr>
2017-09-07 14:02:34 +0200

    added voiture.php and made structure of the array

---
+++ include/header.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html class="no-js" lang="">
+
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <title></title>
+  <meta name="description" content="">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <link rel="icon" href="favicon.png">
+  <link rel="stylesheet" href="css/bootsrap.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.3.2/css/mdb.min.css" />
+  <link rel="stylesheet" href="css/normalize.css">
+  <link rel="stylesheet" href="css/main.css">
+</head>
+
+<body>
+test
---
+++ include/voiture.php
@@ -0,0 +1,9 @@
+<?php
+  $voitures = ["0" => ["modele" => "Porsche Panamera", "type" => "v6 GTS", "image" => "img/image.png"],
+              "1" => ["modele" => "Porsche Panamera", "type" => "v6 GTS", "image" => "img/image.png"],
+              "2" => ["modele" => "Porsche Panamera", "type" => "v6 GTS", "image" => "img/image.png"],
+              "3" => ["modele" => "Porsche Panamera", "type" => "v6 GTS", "image" => "img/image.png"],
+              "4" => ["modele" => "Porsche Panamera", "type" => "v6 GTS", "image" => "img/image.png"],
+              "5" => ["modele" => "Porsche Panamera", "type" => "v6 GTS", "image" => "img/image.png"]];
+
+?>
