65cc458950b8b0aaa99685b3d782d81c8ac00b47
moradsimplon <morad.bzioui@laposte.net>
2017-09-08 09:38:46 +0200

    add margin and change color in card

--- include/carte.php
+++ include/carte.php
@@ -1,10 +1,10 @@
 
 
 <div class="col-lg-4 ">
-  <div class="card-deck mx-auto" style="width:350px">
+  <div class="card-deck mx-auto m-4" style="width:350px">
 
       <!--Card image-->
-      <img class="card-img-top mx-auto" style="width: 20rem;" src="<?php echo $voiture['image']; ?>" alt="<?php echo $voiture['modele']; ?>">
+      <img class="card-img-top mx-auto mt-1" style="width: 20rem;" src="<?php echo $voiture['image']; ?>" alt="<?php echo $voiture['modele']; ?>">
 
       <!--Card content-->
       <div class="card-block">
@@ -14,7 +14,7 @@
           <p class="card-text"><?php echo $voiture['type']; ?></p>
           <form method="post" style="width: 20rem;">
             <input type="hidden" name="id[]" value="<?php echo $id_voiture ?>">
-            <input type="submit" name="submit" value="Display details" class="btn btn-primary">
+            <input id="buttoncard" type="submit" name="submit" value="Display details" class="btn btn">
           </form>
       </div>
   </div>
