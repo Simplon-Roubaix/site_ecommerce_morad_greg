9d984ce6343d92325cb27edc4d9d66fef155898a
Greg Fauconnier <g.fauconnier@hotmail.fr>
2017-09-08 10:03:03 +0200

    changed structure of index.php (removed html and added includes) - details.php : did most of the structure, need to add css

---
+++ include/details.php
@@ -0,0 +1,14 @@
+<?php
+  $id_voiture = $_POST['id'][0];
+?>
+  <div class="">
+    <h2><?php echo $voitures[$id_voiture]['modele']; ?></h2>
+    <h3><?php echo $voitures[$id_voiture]['type']; ?></h3>
+    <img src="<?php echo $voitures[$id_voiture]['image']; ?>" alt="<?php echo $voitures[$id_voiture]['modele']; ?>">
+    <p><?php echo $voitures[$id_voiture]['details']; ?></p>
+
+    <form method="post">
+        <input type="submit" name="submit" value="Retour" class="">
+    </form>
+
+  </div>
--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
 include('include/header.php');
+include('include/manager.php');
 include ('include/footer.php');
 ?>
