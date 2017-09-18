b316858fc98415d174095971794cc285683d1528
Greg Fauconnier <g.fauconnier@hotmail.fr>
2017-09-18 14:43:11 +0200

    admin_post.php : started to add operations condition - sign_manage : typos - modif_vehicule.php : added forms to display actual content of vehicule infos

--- include/admin_post.php
+++ include/admin_post.php
@@ -1,64 +1,57 @@
 <?php
   $err_count = 0;
 
-  if(isset($_POST['marque_v']) AND isset($_POST['modele_v']) AND isset($_POST['descriptif_v']) AND isset($_POST['altimg'])) {
+  if (isset($_POST['operation']) and $_POST['operation'] == 1) {
+      if (isset($_POST['marque_v']) and isset($_POST['modele_v']) and isset($_POST['descriptif_v']) and isset($_POST['altimg'])) {
+          $arr_post[] = $_POST['marque_v'];
+          $arr_post[] = $_POST['modele_v'];
+          $arr_post[] = $_POST['descriptif_v'];
+          $arr_post[] = $_POST['date_v'];
+          $arr_post[] = $_POST['prix_v'];
+          $arr_post[] = $_POST['altimg'];
 
-    $arr_post[] = $_POST['marque_v'];
-    $arr_post[] = $_POST['modele_v'];
-    $arr_post[] = $_POST['descriptif_v'];
-    $arr_post[] = $_POST['date_v'];
-    $arr_post[] = $_POST['prix_v'];
-    $arr_post[] = $_POST['altimg'];
+          foreach ($arr_post as $key => $value) {
+              $value = trim($value);
+              $value = strip_tags($value);
+              $value = htmlspecialchars($value);
+              $arr_post[$key] = $value;
+          }
+      } else {
+          $err_count++;
+      }
 
-    foreach ($arr_post as $key => $value) {
-      $value = trim($value);
-      $value = strip_tags($value);
-      $value = htmlspecialchars($value);
-      $arr_post[$key] = $value;
-    }
-
-  }
-  else {
-    $err_count++;
-  }
-
-  if(isset($_FILES['imgfile']) AND $_FILES['imgfile']['error'] == 0) {
-    if ($_FILES['imgfile']['size'] <= 12800000)
-        {
-                $infosfichier = pathinfo($_FILES['imgfile']['name']);
-                $extension_upload = $infosfichier['extension'];
-                $extensions_autorisees = array('jpg', 'jpeg', 'gif', 'png');
-                if (in_array($extension_upload, $extensions_autorisees))
-                {
+      if (isset($_FILES['imgfile']) and $_FILES['imgfile']['error'] == 0) {
+          if ($_FILES['imgfile']['size'] <= 12800000) {
+              $infosfichier = pathinfo($_FILES['imgfile']['name']);
+              $extension_upload = $infosfichier['extension'];
+              $extensions_autorisees = array('jpg', 'jpeg', 'gif', 'png');
+              if (in_array($extension_upload, $extensions_autorisees)) {
                   $imgsource = '../img/'.basename($_FILES['imgfile']['name']);
                   move_uploaded_file($_FILES['imgfile']['tmp_name'], $imgsource);
-                }
-        }
-        else {
+              }
+          } else {
+              $err_count++;
+          }
+      } else {
           $err_count++;
-        }
-  }
-  else {
-    $err_count++;
-  }
+      }
 
-  if($err_count == 0){
-    $req_v = $bdd->prepare('INSERT INTO vehicules(marque, model, descriptif, annee, prix_de_vente)
+      if ($err_count == 0) {
+          $req_v = $bdd->prepare('INSERT INTO vehicules(marque, model, descriptif, annee, prix_de_vente)
                             VALUES(:marque, :model, :descriptif, :annee, :prixv)');
-    $req_v->execute(array('marque' => $arr_post[0],
+          $req_v->execute(array('marque' => $arr_post[0],
                           'model' => $arr_post[1],
                           'descriptif' => $arr_post[2],
                           'annee' => $arr_post[3],
                           'prixv' => $arr_post[4]));
 
-    $req_idv = $bdd->query('SELECT MAX(id_v) FROM vehicules');
-    $max_idv = $req_idv->fetch();
+          $req_idv = $bdd->query('SELECT MAX(id_v) FROM vehicules');
+          $max_idv = $req_idv->fetch();
 
-    $req_img = $bdd->prepare('INSERT INTO images(id_v, source, alt) VALUES(:idv, :source, :alt)');
-    $req_img->execute(array('idv' => $max_idv[0],
+          $req_img = $bdd->prepare('INSERT INTO images(id_v, source, alt) VALUES(:idv, :source, :alt)');
+          $req_img->execute(array('idv' => $max_idv[0],
                             'source' => $imgsource,
                             'alt' => $arr_post[5]));
+      }
+      header('Location: admin.php');
   }
-
-header('Location: admin.php');
-?>
--- include/modif_vehicule.php
+++ include/modif_vehicule.php
@@ -9,8 +9,8 @@
     $id_v = trim($_GET['id']);
     $id_v = strip_tags($id_v);
     $id_v = htmlspecialchars($id_v);
-    $req = $bdd->query('SELECT * FROM vehicules WHERE id_v = \'' . $id_v . '\'') or die(print_r($bdd->errorInfo()));
-    $resultat = $req->fetchAll();
+    $reqv = $bdd->query('SELECT * FROM vehicules WHERE id_v = \'' . $id_v . '\'') or die(print_r($bdd->errorInfo()));
+    $resultat = $reqv->fetchAll();
 
     if(count($resultat)) {
     ?>
@@ -20,39 +20,45 @@
       <div class="form-group row">
         <label for="marque" class="col-2 col-form-label">Marque</label>
         <div class="col-10">
-          <input class="form-control" type="text" name="marque_v" placeholder="Marque" id="marque" required>
+          <input class="form-control" type="text" name="marque_v" value="<?php echo $resultat[0]['marque']; ?>" placeholder="Marque" id="marque" required>
         </div>
       </div>
 
       <div class="form-group row">
         <label for="modele" class="col-2 col-form-label">Modele</label>
         <div class="col-10">
-          <input class="form-control" type="text" name="modele_v" placeholder="Modele" id="modele" required>
+          <input class="form-control" type="text" name="modele_v" value="<?php echo $resultat[0]['model']; ?>" placeholder="Modele" id="modele" required>
         </div>
       </div>
 
       <div class="form-group row">
         <label for="descriptif" class="col-2 col-form-label">Descriptifs</label>
         <div class="col-10">
-          <input class="form-control" type="text" name="descriptif_v" placeholder="Descriptif" id="descriptif" required>
+          <input class="form-control" type="text" name="descriptif_v" value="<?php echo $resultat[0]['descriptif']; ?>" placeholder="Descriptif" id="descriptif" required>
         </div>
       </div>
 
       <div class="form-group row">
         <label for="datev" class="col-2 col-form-label">Date</label>
         <div class="col-10">
-          <input class="form-control" type="date" name="date_v" placeholder="24/07/2015" id="datev">
+          <input class="form-control" type="date" name="date_v" value="<?php echo $resultat[0]['annee']; ?>" placeholder="24/07/2015" id="datev">
         </div>
       </div>
 
       <div class="form-group row">
         <label for="prixv" class="col-2 col-form-label">Prix</label>
         <div class="col-10">
-          <input class="form-control" type="text" name="prix_v" placeholder="18000€" id="prixv">
+          <input class="form-control" type="text" name="prix_v"  value="<?php echo $resultat[0]['prix_de_vente']; ?>"placeholder="18000€" id="prixv">
         </div>
       </div>
 
-      <div class="form-group row">
+      <?php
+      /*$reqimg = $bdd->query('SELECT * FROM images WHERE WHERE id_v = \'' . $id_v . '\'') or die(print_r($bdd->errorInfo()));
+      $imgres = $reqimg->fetchAll();
+      $lastimg = $imgres[count($imgres)-1];*/
+      ?>
+
+      <!-- <div class="form-group row">
         <label for="imgfile" class="col-2 col-form-label">Image</label>
         <div class="col-10">
           <input class="form-control" type="file" name="imgfile" id="imgfile" required>
@@ -62,11 +68,21 @@
       <div class="form-group row">
         <label for="descimg" class="col-2 col-form-label">Image Alt</label>
         <div class="col-10">
-          <input class="form-control" type="text" name="altimg" placeholder="Alt" id="descimg" required>
+          <input class="form-control" type="text" name="altimg" value="<?php //echo $lastimg['alt']; ?>" placeholder="Alt" id="descimg" required>
         </div>
-      </div>
-      <input type="hidden" name="operation" value="1">
-      <button class="btn btn-lg btn-primary btn-block" type="submit">Enregistrer</button>
+      </div> -->
+
+      <input type="hidden" name="operation" value="2">
+
+      <button class="btn btn-lg btn-primary btn-block" type="submit">Modifier</button>
+
+      </form>
+
+      <form action="admin_post.php" method="post">
+        <input type="hidden" name="operation" value="3">
+        <input type="submit" name="suppr" value="Supprimer">
+      </form>
+
     <?php
     }
     else {
