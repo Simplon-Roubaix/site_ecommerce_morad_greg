b23b55786a1c82db59bcdaba0aac14cf455e8ee3
Greg Fauconnier <g.fauconnier@hotmail.fr>
2017-09-22 09:13:49 +0200

    removed manager content and put it in accueil.php

--- model/deleteproduct.php
+++ model/deleteproduct.php
@@ -9,10 +9,7 @@ function deleteproduct($id_v)
     // also removes the file from img folder
     unlink($img['source']);
 
-    $req_del = $bdd->prepare('DELETE FROM images WHERE id_v = :id_v');
-    $req_del->execute(array('id_v' => $id_v));
-
-    $req_del = $bdd->prepare('DELETE FROM vehicules WHERE id_v = :id_v');
+    $req_del = $bdd->prepare('DELETE FROM vehicules WHERE id_v = :id_v ');
     $req_del->execute(array('id_v' => $id_v));
 
 }
