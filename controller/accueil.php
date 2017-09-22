7637f1bace1b884c54bb339273263047025623f0
Greg Fauconnier <g.fauconnier@hotmail.fr>
2017-09-22 11:25:15 +0200

    added limit to innerjoin query

--- controller/accueil.php
+++ controller/accueil.php
@@ -5,7 +5,16 @@ include '../vue/template/header.php';
 <div class="container">
   <div class="row">
     <?php
-    include 'manager.php';
+    // displays details or all cards depending on what is sent or not
+    if (isset($_POST['id'])) {
+        include('../vue/details.php');
+    } else {
+        $display_cards = req_inner_vehicules_img();
+        $voitures = $display_cards->fetchAll();
+        foreach ($voitures as $voiture) {
+            include('../vue/carte.php');
+        }
+    }
     ?>
   </div>
 </div>
--- model/detailsqueries.php
+++ model/detailsqueries.php
@@ -8,7 +8,7 @@ function req_inner_vehicules_img($id = NULL)
       $id = ' WHERE vehicules.id_v = \'' . $id . '\'';
     }
     // selects content of vehicules and images tables depending on sent id
-    $display_result = $bdd->query('SELECT * FROM vehicules INNER JOIN images ON vehicules.id_v = images.id_v'.$id);
+    $display_result = $bdd->query('SELECT * FROM vehicules INNER JOIN images ON vehicules.id_v = images.id_v'.$id.' ORDER BY vehicules.id_v DESC LIMIT 0,9');
 
     return $display_result;
 }
